582dfaf660cb866e6f6e92a3cc2db7b07a9ef0b8
Katheryn-Caceres-S <kcs.ingcivil@gmail.com>
2025-04-25 03:14:24 +0000

    avance en carrusel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,30 @@
-alert('estoy conectado');
 
-$('#btn').click(function(){
-    alert('Correo Enviado correctamente')
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+/* alert("estoy conectado"); */
+
+
+$("#btn").click(function(){
+    alert("Correo Enviado correctamente")
 });
 
-$('.hover').hover(
-    function(){ $(this).css('background-color', 'yellow') },
-    function(){ $(this).css('background-color', '') },
+$(".hover").hover(
+    function(){ $(this).css("background-color", "yellow") },
+    function(){ $(this).css("background-color", "") },
 )
 
+/*
+$('btntoggle').click(fuction(){
+    $('#contenido').toggle()
+})
+
+/*
 $('h2').dbclick(function){
     $(this).css('color', 'red');
 }
+    */
